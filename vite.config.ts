cd6a2658615a218e747737ee18b2928eeb9b8d7a
abram-kaleb <going.merry1331@gmail.com>
2026-02-27 13:27:59 +0100

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
 })
